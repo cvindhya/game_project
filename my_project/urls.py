c6f34b80346bcd7cd44548b399f9f32701aff254
Vindhya C <cvindhya2@gmail.com>
2023-12-11 15:18:02 +0530

    Latest commit

--- my_project/urls.py
+++ my_project/urls.py
@@ -1,9 +1,10 @@
 from django.contrib import admin 
 from django.urls import path,include
-
+from django.contrib.auth.views import LoginView
 
 urlpatterns = [
-    path('',include('my_app.urls')),
+    path('',LoginView.as_view(), name='login'),
+    path('login',include('my_app.urls')),
     path('admin/', admin.site.urls),
     path('accounts/',include('django.contrib.auth.urls')),
 ]
